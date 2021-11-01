9e275f1db1a34cec3fc77e75a793d33c2d2b3c31
Stefana Matei <stefana.otilia.matei@gmail.com>
2021-11-01 21:04:36 +0200

    initial commit tema5

---
+++ Tema5/Tema5/Tema5/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Tema5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNume = new System.Windows.Forms.Label();
+            this.lblCNP = new System.Windows.Forms.Label();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.txtNume = new System.Windows.Forms.TextBox();
+            this.txtCNP = new System.Windows.Forms.TextBox();
+            this.cmbTest = new System.Windows.Forms.ComboBox();
+            this.btnStartTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNume
+            // 
+            this.lblNume.AutoSize = true;
+            this.lblNume.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNume.Location = new System.Drawing.Point(189, 62);
+            this.lblNume.Name = "lblNume";
+            this.lblNume.Size = new System.Drawing.Size(62, 24);
+            this.lblNume.TabIndex = 1;
+            this.lblNume.Text = "Nume";
+            // 
+            // lblCNP
+            // 
+            this.lblCNP.AutoSize = true;
+            this.lblCNP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCNP.Location = new System.Drawing.Point(189, 121);
+            this.lblCNP.Name = "lblCNP";
+            this.lblCNP.Size = new System.Drawing.Size(49, 24);
+            this.lblCNP.TabIndex = 2;
+            this.lblCNP.Text = "CNP";
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTest.Location = new System.Drawing.Point(189, 193);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(46, 24);
+            this.lblTest.TabIndex = 3;
+            this.lblTest.Text = "Test";
+            // 
+            // txtNume
+            // 
+            this.txtNume.Location = new System.Drawing.Point(327, 64);
+            this.txtNume.Name = "txtNume";
+            this.txtNume.Size = new System.Drawing.Size(347, 22);
+            this.txtNume.TabIndex = 4;
+            // 
+            // txtCNP
+            // 
+            this.txtCNP.Location = new System.Drawing.Point(327, 123);
+            this.txtCNP.Name = "txtCNP";
+            this.txtCNP.Size = new System.Drawing.Size(347, 22);
+            this.txtCNP.TabIndex = 5;
+            this.txtCNP.Leave += new System.EventHandler(this.txtCNP_Leave);
+            // 
+            // cmbTest
+            // 
+            this.cmbTest.FormattingEnabled = true;
+            this.cmbTest.Location = new System.Drawing.Point(327, 193);
+            this.cmbTest.Name = "cmbTest";
+            this.cmbTest.Size = new System.Drawing.Size(183, 24);
+            this.cmbTest.TabIndex = 6;
+            // 
+            // btnStartTest
+            // 
+            this.btnStartTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartTest.Location = new System.Drawing.Point(367, 269);
+            this.btnStartTest.Name = "btnStartTest";
+            this.btnStartTest.Size = new System.Drawing.Size(143, 39);
+            this.btnStartTest.TabIndex = 7;
+            this.btnStartTest.Text = "Incepe testul";
+            this.btnStartTest.UseVisualStyleBackColor = true;
+            this.btnStartTest.Click += new System.EventHandler(this.btnStartTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 330);
+            this.Controls.Add(this.btnStartTest);
+            this.Controls.Add(this.cmbTest);
+            this.Controls.Add(this.txtCNP);
+            this.Controls.Add(this.txtNume);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.lblCNP);
+            this.Controls.Add(this.lblNume);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNume;
+        private System.Windows.Forms.Label lblCNP;
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.TextBox txtNume;
+        private System.Windows.Forms.TextBox txtCNP;
+        private System.Windows.Forms.ComboBox cmbTest;
+        private System.Windows.Forms.Button btnStartTest;
+    }
+}
+
---
+++ Tema5/Tema5/Tema5/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace Tema5
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] xmlFiles = Directory.GetFiles(Application.StartupPath, "*.xml");
+
+
+            foreach(string xmlFile in xmlFiles)
+            {
+                cmbTest.Items.Add(Path.GetFileName(xmlFile).Split('.')[0]);
+            }
+        }
+
+
+        private void txtCNP_Leave(object sender, EventArgs e)
+        {
+            if (verificareCNP(txtCNP.Text.Trim()) == true)
+            {
+                MessageBox.Show("CNP este valid!");
+            }
+            else
+            {
+                MessageBox.Show("CNP nu este valid!");
+            }
+        }
+
+
+        public static bool verificareCNP(string cnp)
+        {
+
+            int sex, an1, an2, luna1, luna2, ziua1, ziua2, judet1, judet2,
+                            n1, n2, n3, cifraControl, cCNP;
+
+            if (cnp.Trim().Length != 13)
+            {
+                return false;
+            }
+            else
+            {
+                sex = Convert.ToInt32(cnp.Substring(0, 1));
+                an1 = Convert.ToInt32(cnp.Substring(1, 1));
+                an2 = Convert.ToInt32(cnp.Substring(2, 1));
+                luna1 = Convert.ToInt32(cnp.Substring(3, 1));
+                luna2 = Convert.ToInt32(cnp.Substring(4, 1));
+                ziua1 = Convert.ToInt32(cnp.Substring(5, 1));
+                ziua2 = Convert.ToInt32(cnp.Substring(6, 1));
+                judet1 = Convert.ToInt32(cnp.Substring(7, 1));
+                judet2 = Convert.ToInt32(cnp.Substring(8, 1));
+                n1 = Convert.ToInt32(cnp.Substring(9, 1));
+                n2 = Convert.ToInt32(cnp.Substring(10, 1));
+                n3 = Convert.ToInt32(cnp.Substring(11, 1));
+                cifraControl = Convert.ToInt32(((sex * 2 + an1 * 7
+                    + an2 * 9 + luna1 * 1 + luna2 * 4 + ziua1 * 6
+                    + ziua2 * 3 + judet1 * 5 + judet2 * 8 + n1 * 2
+                    + n2 * 7 + n3 * 9) % 11));
+
+
+                if (cifraControl == 10)
+                {
+                    cifraControl = 1;
+                }
+
+
+                cCNP = Convert.ToInt32(cnp.Substring(12, 1));
+
+                if (cifraControl == cCNP)
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        private void btnStartTest_Click(object sender, EventArgs e)
+        {
+            string nume = txtNume.Text;
+            string test = cmbTest.Text;
+            Form2 form2 = new Form2(nume, test);
+            form2.ShowDialog();
+        }
+    }
+}
