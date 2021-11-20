aa86277b0c78663d5aff04ec2a163930eeafc2ce
Stefana Matei <stefana.otilia.matei@gmail.com>
2021-11-20 19:48:27 +0200

    Update Chestionar.aspx.designer.cs

--- Tema8/Tema8/Tema8/Chestionar.aspx.designer.cs
+++ Tema8/Tema8/Tema8/Chestionar.aspx.designer.cs
@@ -33,15 +33,6 @@ namespace Tema8
         protected global::System.Web.UI.WebControls.Label lblChestionar;
 
         /// <summary>
-        /// txtRaspunsuriChestionar control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtRaspunsuriChestionar;
-
-        /// <summary>
         /// PlaceHolder1 control.
         /// </summary>
         /// <remarks>
